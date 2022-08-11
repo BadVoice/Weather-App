930f164ab012cf7f2f9c35dd646d29df03774ad1
Herman <smurovnemez@mail.ru>
2022-08-11 15:42:11 +0300

    feat: add layout component SiteNavigation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup>
   import { RouterView } from 'vue-router';
+  import SiteNavigation from '@/components/layout/SiteNavigation.vue';
 </script>
-
-<style lang="scss" scoped>
-
-</style>
