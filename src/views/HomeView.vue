be92d6a60a4b0cb4955516d7e714616a2ebc41cc
Herman <smurovnemez@mail.ru>
2022-08-15 19:01:23 +0300

    feat: city search with API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,53 @@
+<template>
+  <main class="container text-white ">
+    <div class="pt-4 mb-8 relative">
+      <input 
+      type="text" 
+      v-model="searchQuery"
+      @input="getSearchResults"
+      placeholder="Search for a city or state"
+      class="py-2 px-1 w-full bg-transparent border-b 
+      focus:border-weather-secondary focus:outline-none
+      focus:shadow-[0px_1px_0_0_#004E71]"
+      >
+      <ul class="absolute bg-weather-secondary text-white
+      w-full shadow-md py-2 px-1 top-[66px]"
+      v-if="weatherSearchResults"
+      > 
+        <li v-for="searchResult in weatherSearchResults" :key="searchResult.id"
+        class="py-2 cursor-pointer">
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
+    </div>
+  </main>
+</template>
+
+
 <script setup>
+import { ref } from "@vue/reactivity";
+import axios from 'axios'
+
+    const weatherAPIKey = 'pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFzODZvMHJkaDJ1bWx6OGVieGxreSJ9.IpojdT3U3NENknF6_WhR2Q'
+    const searchQuery = ref('')
+    const queryTimeout = ref(null)
+    const weatherSearchResults = ref(null)
+
+    const getSearchResults = () => {
+      clearTimeout(queryTimeout.value)
+      queryTimeout.value = setTimeout(async () => {
+        if (searchQuery.value !== '') {
+          const result  = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${weatherAPIKey}&types=place`
+          );
+          weatherSearchResults.value = result.data.features
+          console.log(weatherSearchResults.value)
+          return;
+        }
+        weatherSearchResults.value = null
+      }, 300)
+    }
+
+
 
 </script>
 
-<template>
-  <main>
-  
-  </main>
-</template>
