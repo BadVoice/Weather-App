3593de157431c37db9e753e7bebed65eeaa1c61b
Herman <smurovnemez@mail.ru>
2022-08-18 23:38:21 +0300

    feat: handling API errors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,40 @@
   <main class="container text-white ">
     <div class="pt-4 mb-8 relative">
       <input 
-      type="text" 
-      v-model="searchQuery"
-      @input="getSearchResults"
-      placeholder="Search for a city or state"
-      class="py-2 px-1 w-full bg-transparent border-b 
-      focus:border-weather-secondary focus:outline-none
-      focus:shadow-[0px_1px_0_0_#004E71]"
+        type="text" 
+        v-model="searchQuery"
+        @input="getSearchResults"
+        placeholder="Search for a city or state"
+        class="py-2 px-1 w-full bg-transparent border-b 
+        focus:border-weather-secondary focus:outline-none
+        focus:shadow-[0px_1px_0_0_#004E71]"
       >
-      <ul class="absolute bg-weather-secondary text-white
-      w-full shadow-md py-2 px-1 top-[66px]"
-      v-if="weatherSearchResults"
+
+      <ul 
+        class="absolute bg-weather-secondary text-white
+        w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="weatherSearchResults"
       > 
-        <li v-for="searchResult in weatherSearchResults" :key="searchResult.id"
-        class="py-2 cursor-pointer">
-          {{ searchResult.place_name }}
-        </li>
+      <p 
+        class="py-2"
+        v-if="searchError">
+        Sorry, something went wrong, please try again.
+      </p>
+
+      <p 
+        class="py-2"
+        v-if="!searchError && weatherSearchResults.length === 0">
+        No results match your query, try a different term. 
+      </p>
+
+        <template v-else>
+          <li v-for="searchResult in weatherSearchResults" 
+          :key="searchResult.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ searchResult.place_name }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -32,15 +50,22 @@ import axios from 'axios'
     const searchQuery = ref('')
     const queryTimeout = ref(null)
     const weatherSearchResults = ref(null)
+    const searchError = ref(null)
 
     const getSearchResults = () => {
       clearTimeout(queryTimeout.value)
       queryTimeout.value = setTimeout(async () => {
+
         if (searchQuery.value !== '') {
-          const result  = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${weatherAPIKey}&types=place`
-          );
-          weatherSearchResults.value = result.data.features
-          console.log(weatherSearchResults.value)
+            try {
+              const result  = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${weatherAPIKey}&types=place`
+              );
+              weatherSearchResults.value = result.data.features
+              } catch {
+                searchError.value = true
+              }
+          
+          
           return;
         }
         weatherSearchResults.value = null
