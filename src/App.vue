a81ab6d83045543178dee5da229175ee12be84d8
Herman <smurovnemez@mail.ru>
2022-08-29 20:59:13 +0300

    feat: route transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
     <SiteNavigation />
-    <RouterView />
+    <RouterView v-slot="{ Component } "> 
+        <Transition name="page" mode="out-in">
+          <component :is="Component" />
+        </Transition>
+    </RouterView>
   </div>
 </template>
 
@@ -9,3 +13,18 @@
   import { RouterView } from 'vue-router';
   import SiteNavigation from '@/components/layout/SiteNavigation.vue';
 </script>
+
+<style>
+  .page-enter-active,
+  .page-leave-active {
+    transition: 600ms ease all;
+  }
+
+  .page-enter-from,
+  .page-leave-to{
+    opacity: 0;
+  }
+
+
+
+</style>
